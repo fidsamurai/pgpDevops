f92a2c34ea15c8ee6f6fbb9a4ffdae59c41ae970
fidsamurai <firdausbeckham23@gmail.com>
2022-03-19 22:53:46 +0530

    Ec2 attempt

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -14,9 +14,28 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_security_group" "SSH" {
+  name = "SSH Fid Home"
+  vpc = aws_vpc.main.id
+
+  ingress {
+    description = "SSH Fid"
+    from_port = "22"
+    to_port = "22"
+    protocol = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  tags = {
+    Name = "devops"
+  }
+}
+
 resource "aws_instance" "pgpDemo" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3a.micro"
+  key_name = "devops"
+  vpc_security_group_ids = aws_security_group.SSH.id
 
   tags = {
     Name = "pgpDevops"
--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -1,8 +1,6 @@
-provider "aws" {
 resource "s3" "pgpDevops-fid" {
   tags = {
     Name = "pgpDevops-fid"
     Env = "pgpDevops"
   }
-}
 }
